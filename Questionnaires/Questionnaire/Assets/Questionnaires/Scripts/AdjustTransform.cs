9ae89daef66771914594e6cc151b446352ca30c1
Z.Q. Chen <906355844@qq.com>
2020-11-08 18:02:44 +0100

    Updated AdjustTransform.cs
    - namespace added
    - comments added

--- Questionnaires/Questionnaire/Assets/Questionnaires/Scripts/AdjustTransform.cs
+++ Questionnaires/Questionnaire/Assets/Questionnaires/Scripts/AdjustTransform.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AdjustTransform : MonoBehaviour
+namespace VRQuestionnaireToolkit
 {
-    void Update()
+    // Resizing the Questionnaire interface by hitting keys + and -
+    public class AdjustTransform : MonoBehaviour
     {
-        // Press + to scale up
-        if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadPlus))
-            this.transform.localScale = Vector3.Scale(this.transform.localScale, new Vector3(1.1f, 1.1f, 1.0f));
-        // Press - to scale down
-        if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus))
-            this.transform.localScale = Vector3.Scale(this.transform.localScale, new Vector3(0.9f, 0.9f, 1.0f));
-        // Press 0 to reset
-        if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
-            this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        void Update()
+        {
+            // Press + to scale up
+            if (Input.GetKeyDown(KeyCode.Equals) || Input.GetKeyDown(KeyCode.KeypadPlus))
+                this.transform.localScale = Vector3.Scale(this.transform.localScale, new Vector3(1.1f, 1.1f, 1.0f));
+            // Press - to scale down
+            if (Input.GetKeyDown(KeyCode.Minus) || Input.GetKeyDown(KeyCode.KeypadMinus))
+                this.transform.localScale = Vector3.Scale(this.transform.localScale, new Vector3(0.9f, 0.9f, 1.0f));
+            // Press 0 to reset
+            if (Input.GetKeyDown(KeyCode.Alpha0) || Input.GetKeyDown(KeyCode.Keypad0))
+                this.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        }
     }
 }
